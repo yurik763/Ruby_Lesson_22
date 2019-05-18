3907a16ec100c473fd7091688eebc58ec2b18737
Yuri <yurik763@tut.by>
2019-05-18 19:50:46 +0300

    Add users.txt

--- app.rb
+++ app.rb
@@ -15,6 +15,23 @@ get '/visit' do
   erb :visit
 end
 
+post '/visit' do
+  # user_name, phone, date_time
+  @username = params[:username]
+  @phone = params[:phone]
+  @date_time = params[:date_time]
+
+  @title = "Спасибо!"
+  @message = "Уважаемый #{@username}, мы ждём вас #{@date_time}"
+
+  # запишем в файл то, что ввёл клиент
+  f = File.open './public/users.txt', 'a'
+  f.write "Имя: #{@username}, телефон: #{@phone}, дата и время: #{@date_time}.\n"
+  f.close
+
+  erb :message
+end
+
 get '/contacts' do
   erb :contacts
 end
